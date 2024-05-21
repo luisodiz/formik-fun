1d250c0654af7232a2ad48ce52177e2d73422530
Alexander Sosov <a.sosov@vlig44.ru>
2024-05-21 14:40:25 +0300

    feat: использовать material ui и material icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Icon from '@mui/material/Icon'
+import Button from '@mui/material/Button'
+
 const App = () => {
-  return <div>React Formik Fun</div>
+  return (
+    <div>
+      React Formik Fun<Icon>star</Icon>
+      <Button variant="contained">Contained</Button>
+    </div>
+  )
 }
 
 export default App
