d53d9c7fbeaf67b1031ada8e1dcbcbed2eaed800
Alexander Sosov <a.sosov@vlig44.ru>
2024-05-22 16:00:20 +0300

    chore

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,37 @@
+import {Paper, Container, Button, Grid} from '@mui/material'
+import {useNavigate} from 'react-router-dom'
+
+import Logo from '../Logo/Logo.tsx'
+
+const Header = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Paper
+      variant="elevation"
+      square={true}
+      elevation={2}
+      component="header"
+      sx={{py: '10px'}}
+    >
+      <Container>
+        <Grid container alignItems="center">
+          <Grid xs={4}>
+            <Logo />
+          </Grid>
+          <Grid xs={8} container justifyContent="flex-end">
+            <Button
+              variant="contained"
+              onClick={() => navigate('/sign-in')}
+              size="large"
+            >
+              Войти
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </Paper>
+  )
+}
+
+export default Header
