6b160d2cfdd3861424e46380304f70cf1bad9a2a
Alexander Sosov <a.sosov@vlig44.ru>
2024-05-23 16:59:30 +0300

    fix: изменить параметры видимости подключения к firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 import {initializeApp} from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.AUTH_DOMAIN,
   projectId: import.meta.env.PROJECT_ID,
   storageBucket: import.meta.env.STORAGE_BUCKET,
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly API_KEY: string
+  readonly VITE_API_KEY: string
   readonly AUTH_DOMAIN: string
   readonly PROJECT_ID: string
   readonly STORAGE_BUCKET: string
