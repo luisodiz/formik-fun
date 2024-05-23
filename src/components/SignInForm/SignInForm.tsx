50c1b6d96f61ce4c1efa5c4eabe3fc23cbebcc82
Alexander Sosov <a.sosov@vlig44.ru>
2024-05-23 14:35:32 +0300

    feat: добавить схему валидации формы авторизации

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -1,5 +1,17 @@
 import {TextField, Button} from '@mui/material'
 import {Formik, Form, Field} from 'formik'
+import * as yup from 'yup'
+
+const SignInFormSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Неверный формат email адреса')
+    .required('Поле обязательное'),
+  password: yup
+    .string()
+    .min(6, 'Пароль должен быть не менее 6-ти символов')
+    .required('Поле обязательное'),
+})
 
 const SignInForm = () => {
   return (
@@ -8,8 +20,9 @@ const SignInForm = () => {
       onSubmit={(values) => {
         console.log(values)
       }}
+      validationSchema={SignInFormSchema}
     >
-      {({handleChange}) => (
+      {({handleChange, handleBlur, errors, touched}) => (
         <Form>
           <Field
             component={TextField}
@@ -18,6 +31,10 @@ const SignInForm = () => {
             size="small"
             sx={{mb: 2}}
             onChange={handleChange('email')}
+            onBlur={handleBlur('email')}
+            name="email"
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
           />
           <Field
             component={TextField}
@@ -26,6 +43,10 @@ const SignInForm = () => {
             size="small"
             sx={{mb: 2}}
             onChange={handleChange('password')}
+            onBlur={handleBlur('password')}
+            name="password"
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
           />
           <Button fullWidth variant="contained" size="large" type="submit">
             Отправить
